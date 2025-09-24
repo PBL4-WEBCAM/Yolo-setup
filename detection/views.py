73ef6077d48ee6feafc3d0c62071d1af56975361
luan <kieutran.142a@gmaill.com>
2025-09-24 19:51:37 +0700

    update project files

--- detection/views.py
+++ detection/views.py
@@ -25,7 +25,7 @@ def gen_frames():
 
                     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,255,0), 2)
                     cv2.putText(frame, f"{label} {conf:.2f}", (int(x1), int(y1)-10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
 
             # Encode JPEG
             ret, buffer = cv2.imencode('.jpg', frame)
