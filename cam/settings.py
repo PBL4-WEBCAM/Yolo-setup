53cc5991c4d88dc01ac299ef3afc8109e4373978
luan <kieutran.142a@gmail.com>
2025-10-10 15:35:12 +0700

    change dection to connect .NET App

--- cam/settings.py
+++ cam/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'detection',
+'corsheaders',
 ]
 
 MIDDLEWARE = [
+'corsheaders.middleware.CorsMiddleware',  # Thêm dòng này lên đầu
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'cam.urls'
 
 TEMPLATES = [
